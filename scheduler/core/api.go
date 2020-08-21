03f447c0c65658769797e12a48a7ad0a142d34e5
hongtao <hongtao.fu@qq.com>
2020-08-21 11:25:34 +0800

    移除无用的代码

--- scheduler/core/api.go
+++ scheduler/core/api.go
@@ -10,10 +10,9 @@ import (
 	Acquire: 获取想要个的container
 	Return: 归还container
 */
+
 var RequestMap map[string]*Container
 var RequestMapLock sync.Mutex
-
-/*********************************** Acquire and Return 相关api *************************************/
 
 //获取一个container
 func Acquire(req *spb.AcquireContainerRequest) *spb.AcquireContainerReply {
@@ -75,7 +74,7 @@ func Acquire(req *spb.AcquireContainerRequest) *spb.AcquireContainerReply {
 	}
 }
 
-//container
+//归还container
 func Return(req *spb.ReturnContainerRequest) {
 	requestId := req.RequestId
 
@@ -86,99 +85,3 @@ func Return(req *spb.ReturnContainerRequest) {
 
 	container.ConcurrencyCount = 2 * 1024 * 1024 * 1024 / req.MaxMemoryUsageInBytes
 }
-
-/*********************************** other 相关api *************************************/
-
-//
-////node和container的关系结构，在申请和归还的时候会用上
-//type NC struct {
-//	Node      *Node
-//	Container *Container
-//}
-//
-////请求表，用于存放所有的请求
-////var RequestMap = make(map[string]*NC)
-//
-////var RequestMapLock sync.Mutex
-//
-////获取一个node里面的container
-//func Acquire(req *pb.AcquireContainerRequest) *pb.AcquireContainerReply {
-//	requestId := req.RequestId
-//	funcName := req.FunctionName
-//	reqMem := req.FunctionConfig.MemoryInBytes
-//
-//	var node *Node
-//	var count int64 //表示container的会用数量
-//	var container *Container
-//
-//	NodesLock.Lock()
-//	//发现一个满足要求的container，且使用人数是最少的container
-//	size := len(nodes)
-//	s := rand.Intn(size) //随机选择一个开始位置
-//	for i := 0; i < size; i++ {
-//		p := (i + s) % size
-//		n := nodes[p]
-//		satisfy, usedCount := n.Satisfy(funcName, reqMem)
-//		if !satisfy { //如果不满足直接返回
-//			continue
-//		}
-//
-//		//如果node为null，就直接赋值
-//		if node == nil {
-//			node, count = n, usedCount
-//			continue
-//		}
-//
-//		//如果使用数量少，就替换
-//		if usedCount < count {
-//			node, count = n, usedCount
-//		}
-//	}
-//	NodesLock.Unlock()
-//	//如果没有找到合适的node，就返回nil
-//	if node == nil {
-//		return nil
-//	}
-//
-//	//获取container
-//	container = node.Acquire(funcName, reqMem)
-//
-//	//记录请求
-//	RequestMapLock.Lock()
-//	RequestMap[requestId] = &NC{node, container}
-//	RequestMapLock.Unlock()
-//
-//	return &pb.AcquireContainerReply{
-//		NodeId:          node.NodeID,
-//		NodeAddress:     node.Address,
-//		NodeServicePort: node.Port,
-//		ContainerId:     container.Id,
-//	}
-//}
-//
-//var count = 0
-//
-////归还node中的container
-//func Return(req *pb.ReturnContainerRequest) {
-//	requestId := req.RequestId
-//
-//	RequestMapLock.Lock()
-//	nc := RequestMap[requestId]
-//	delete(RequestMap, requestId)
-//	RequestMapLock.Unlock()
-//
-//	if nc == nil {
-//		return
-//	}
-//
-//	node := nc.Node
-//	container := nc.Container
-//	//******************log*************************
-//	//if count%100 == 0 {
-//	//	PrintNodes("timer")
-//	//}
-//	//count++
-//	//******************log*************************
-//	actualUseMem := req.MaxMemoryUsageInBytes
-//	node.Return(container, actualUseMem)
-//}
--- scheduler/core/container.go
+++ scheduler/core/container.go
@@ -8,8 +8,6 @@ import (
 var DefaultMaxUsedCount int64 = 1 //Container实例的默认最大连接数
 var CollectionMaxCapacity = 1     //集合最大容量
 
-
-
 //表示一个函数实例
 //存放container信息
 type Container struct {
@@ -36,75 +34,3 @@ func (c *Container) updateContainerStats(stats *pb.ContainerStats) {
 	c.UsageMem = stats.MemoryUsageInBytes
 	c.CpuUsagePct = stats.CpuUsagePct
 }
-
-//
-////向集合中添加一个Container
-//func (cs *Collection) AddContainer(container *Container) {
-//	cs.Containers = append(cs.Containers, container)
-//}
-//
-////判断集合是否还有空间装container实例
-//func (cs *Collection) Lack() bool {
-//	return int64(len(cs.Containers)) < cs.Capacity
-//}
-//
-////判断节点是否满足container的要求,和这个collection的使用人数
-//func (cs *Collection) Satisfy(reqMem int64) (bool, int64) {
-//	//判断集合中是否有容器
-//	if len(cs.Containers) <= 0 {
-//		return false, 0
-//	}
-//	//如果集合的使用人数，小于集合的最大使用人数，就数名满足需要
-//	bool := cs.UsedCount < int64(len(cs.Containers))*cs.MaxUsedCount
-//	return bool, cs.UsedCount
-//}
-//
-////获取container
-//func (cs *Collection) Acquire(reqMem int64) *Container {
-//	//判断集合中是否有容器
-//	if len(cs.Containers) <= 0 {
-//		return nil
-//	}
-//
-//	//获取一个使用人数最少的容器
-//	container := cs.Containers[0]
-//	for _, c := range cs.Containers {
-//		if c.UsedCount < container.UsedCount {
-//			container = c
-//		}
-//	}
-//
-//	cs.UsedCount++
-//	container.UsedCount++
-//	return container
-//}
-//
-////归还container实例
-//func (cs *Collection) Return(container *Container, actualUseMem int64) {
-//	cs.UsedCount--
-//	container.UsedCount--
-//	if actualUseMem == 0 {
-//		actualUseMem = 1 * 1024 * 1024
-//	}
-//	cs.MaxUsedCount = cs.MaxUsedMem / actualUseMem
-//	container.MaxUsedCount = container.MaxUsedMem / actualUseMem
-//	cs.UsedMem = actualUseMem
-//	container.UsedMem = actualUseMem
-//}
-//
-////将collection转换为字符串，打印日志的时候需要，
-//func (cs *Collection) ToString() string {
-//	info := "{" + cs.FuncName + ", " + str(cs.UsedCount) + "/" + str(int64(len(cs.Containers))*cs.MaxUsedCount) +
-//		", " + str(cs.UsedMem/1024/1024) + "/" + str(cs.MaxUsedMem/1024/1024) + ", "
-//
-//	for _, c := range cs.Containers {
-//		//info += "[" + c.FuncName + ", " + str(c.UsedCount) + ", " + str(c.UsedMem/1024/1024) + "], "
-//		info += "[" + str(c.UsedCount) + "/" + str(c.MaxUsedCount) + ", " + str(c.UsedMem/1024/1024) + "], "
-//	}
-//	info += "}"
-//	return info
-//}
-//
-//func str(i int64) string {
-//	return strconv.FormatInt(i, 10)
-//}
